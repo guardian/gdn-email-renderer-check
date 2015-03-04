6ffd04d5a7b4bfdbf2c056a3e7373bc8ad2d219b
Robert Rees <robert.rees@guardian.co.uk>
2015-03-04 16:01:19 +0000

    Creates a version of the check script that uses futures

---
+++ future_checkers.py
@@ -0,0 +1,31 @@
+
+import requests
+import futures
+
+urls = [
+	'http://gu-email-renderer.appspot.com/daily-email-us/v6',
+	'http://gu-email-renderer.appspot.com/daily-email/v1',
+	'http://gu-email-renderer.appspot.com/daily-email-aus/v1',
+	'http://gu-email-renderer.appspot.com/comment-is-free/v1',
+	'http://gu-email-renderer.appspot.com/fashion-statement/v1',
+	'http://gu-email-renderer.appspot.com/zip-file/v1',
+	'http://gu-email-renderer.appspot.com/bookmarks/v1',
+]
+
+failed_urls = []
+
+with futures.ThreadPoolExecutor(max_workers=3) as executor:
+	url_reads = [executor.submit(lambda url: requests.get(url), url) for url in urls]
+
+	for read in url_reads:
+		r = read.result()
+
+		if not r.status_code == 200:
+			failed_urls.append(url)
+
+if not failed_urls:
+	print 'All urls okay'
+else:
+	print 'Failing urls found'
+	print failed_urls
+	exit(1)
